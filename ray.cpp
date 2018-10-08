b4ebc77a44a65032af0fb85205dcc7b5a9407404
Saulo Guilhermino Ferreira Lima <sgfl@ug3c10.windows.cin.ufpe.br>
2018-10-08 12:25:56 -0300

    Classe Ray implementada

---
+++ ray.cpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "Vec3.h"
+#include <float.h>
+
+class Ray{
+
+    private:
+        Vec3 origin, direction;
+
+    public:
+        mutable double tmin = DBL_EPSILON, tmax = DBL_MAX;
+
+        Ray(Vec3 origin, Vec3 direction);
+
+        Vec3 sample (const double& t) const;
+
+        Vec3 getOrigin() const;
+
+        Vec3 getDirection() const;
+}
