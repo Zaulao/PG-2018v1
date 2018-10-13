4539b30e029cc8368962d752173cfad82e1bc94e
luan.brito7 <lab7@cin.ufpe.br>
2018-10-13 22:54:38 +0000

    add image class

--- image.cpp
+++ image.cpp
@@ -1,17 +1,27 @@
 #include <vector>
-#include "Vec3.h"
+#include "Vec3.hpp"
 
 using namespace std;
 
 class Image{
 	private:
 		int width, height;
-		vector <Vec3> buffer;
+		vector <Vec3<int> > buffer;
 	
 	public:
-		Image(int width, int height);
+		Image(int width, int height) {
+			this->width = width;
+			this->height = height;
+			buffer.reserve(width * height);
+		}
 
-		void SetPixel(int x, int y, const Vec3& color);
+		void SetPixel(int x, int y, const Vec3<int> &color) {
+			buffer[ y * width + x] = color;
+		}
+
+		Vec3<int> GetPixel(int x, int y) {
+			return buffer[ y * width + x];
+		}
 
 		void SaveAsPBM(const string& filePath) const;
 
