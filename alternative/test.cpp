dc1473515ac35b9ca5b07969d75f3d36c64f13ff
Eden Santana <eeas@cin.ufpe.br>
2018-11-28 22:30:31 -0300

    leitura do arquivo rodando topmente porém ainda não funciona para vários objetos e também já substituido os valores lidos pelos valores que devem ser colocados na camera

--- alternative/test.cpp
+++ alternative/test.cpp
@@ -4,6 +4,8 @@
 #include "camera.cpp"
 #include "ray.cpp"
 #include "cmath"
+#include <fstream>
+#include <map>
 
 using namespace std;
 typedef Vec3 <double> vec3;
@@ -85,7 +87,7 @@ bool hit (Ray *r, vector <Sphere> &objetosCena, double tmax, Hit_record &rec, ve
         colores = colores.operator+(perc);
         especular = specular(light, record, r, objetosCena.at(index));
         especular = especular.operator*(objetosCena.at(index).getMaterial()->getKs());
-        especular.display();
+       // especular.display();
         colores = colores.operator+(especular);
     if (notShadow) {
         //vec3 light(10, -15, -5);
@@ -110,10 +112,137 @@ vec3 color(Ray *r, vector <Sphere> &objetosCena, Sphere light, vec3 coloures) {
 
 
 int main(){
+
+    fstream reader;
+    reader.open("file.txt");
+    string s;
+    map <string, double> res;
+    map <string, double> camera;
+    map <string, double> material;
+    string material_name;
+    string material_name_sphere;
+    map <string, double> objetos;
+
+    reader >> s;
+    while(reader.good()){
+        reader >> s;
+        if(s == "#resolução"){
+            //2 e 3
+            reader >> s;
+            for(int i = 2; i <=3; i++){
+                reader >> s;
+                switch(i){
+                    case 2:
+                        res["w"] = atof(s.c_str());
+                        break;
+                    case 3:
+                        res["h"] = atof(s.c_str());
+                        break;
+                }       
+            }
+        }else if(s == "#câmera"){
+            // 2 a 12
+            reader >> s;
+            for(int i = 2; i <= 12; i++){
+                reader >> s;
+                switch(i){
+                    case 2:
+                        camera["px"] = atof(s.c_str());
+                        break;
+                    case 3:
+                        camera["py"] = atof(s.c_str());
+                        break;
+                    case 4:
+                        camera["pz"] = atof(s.c_str());
+                        break;
+                    case 5:
+                        camera["tx"] = atof(s.c_str());
+                        break;
+                    case 6:
+                        camera["ty"] = atof(s.c_str());
+                        break;
+                    case 7:
+                        camera["tz"] = atof(s.c_str());
+                        break;
+                    case 8:
+                        camera["ux"] = atof(s.c_str());
+                        break;
+                    case 9:
+                        camera["uy"] = atof(s.c_str());
+                        break;
+                    case 10:
+                        camera["uz"] = atof(s.c_str());
+                        break;
+                    case 11:
+                        camera["fov"] = atof(s.c_str());
+                        break;
+                    case 12:
+                        camera["f"] = atof(s.c_str());
+                        break;
+
+                }
+            }
+        }else if(s == "#materiais"){
+            //2 a 8
+            reader >> s;
+            for(int i = 2; i <= 9; i++){
+                reader >> s;
+                switch(i){
+                    case 2: 
+                        material_name = s;
+                        break;
+                    case 3:
+                        material["r"] = atof(s.c_str()); 
+                        break;
+                    case 4:
+                        material["g"] = atof(s.c_str()); 
+                        break;
+                    case 5:
+                        material["b"] = atof(s.c_str()); 
+                        break;
+                    case 6:
+                        material["kd"] = atof(s.c_str()); 
+                        break;
+                    case 7:
+                        material["ks"] = atof(s.c_str()); 
+                        break;
+                    case 8:
+                        material["ke"] = atof(s.c_str()); 
+                        break;
+                    case 9:
+                        material["alpha"] = atof(s.c_str());
+                }
+            }
+        }else if(s == "#objetos"){
+            //2 a 6
+            reader >> s;
+            for(int i = 2; i <= 6; i++){
+                reader >> s;
+                switch(i){
+                    case 2:
+                        objetos["cx"] = atof(s.c_str());
+                        break;
+                    case 3:
+                        objetos["cy"] = atof(s.c_str());
+                        break;
+                    case 4:
+                        objetos["cz"] = atof(s.c_str());
+                        break;
+                    case 5:
+                        objetos["r"] = atof(s.c_str());
+                        break;
+                    case 6:
+                        material_name_sphere = s;
+                        break;
+                }
+            }
+
+        }
+    }
     
-    vec3 camPos(0, 0, 10);
-    vec3 camTarget(0, 0, -1);
-    vec3 camUp(0, 1, 0);
+    vec3 camPos(camera["px"], camera["py"], camera["pz"]);
+    vec3 camTarget(camera["tx"], camera["ty"], camera["tz"]);
+    vec3 camUp(camera["ux"], camera["uy"], camera["uz"]);
 
     vec3 Ecenter (-5, -3, -50); //red
     vec3 Ecenter2(5, -3, -4); //blue
@@ -135,7 +264,7 @@ int main(){
     Material *material4 = new Material(0,0,1,1, brown);
     Material *luz = new Material(1,1,1,1, white);
 
-    int fov = 75;
+    int fov = camera["fov"];
     double aspect = 1.7;
     double near = 1;
     Camera *cam = new Camera(camPos, camTarget, camUp, fov, near, aspect);
